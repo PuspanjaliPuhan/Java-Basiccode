1a3a94b3dace7e01990d7c328423a5866a5a9322
Puspanjali Puhan <125045123+PuspanjaliPuhan@users.noreply.github.com>
2023-04-30 10:16:47 +0530

    Add files via upload

---
+++ HideAndSeek.java
@@ -0,0 +1,9 @@
+public class HideAndSeek{
+    public static void main(String [] args){
+        System.out.println("Lets play hide and seek.");
+        System.out.println("Three....");
+        System.out.println("Two....");
+        System.out.println("One....");
+        System.out.println("Ready or not , Here i come .");
+    }
+}
