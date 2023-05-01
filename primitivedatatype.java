41d14840a22e29b50ad3f1fc02fecdc1bf96b0a6
Puspanjali Puhan <125045123+PuspanjaliPuhan@users.noreply.github.com>
2023-05-01 23:09:10 +0530

    Add files via upload

---
+++ primitivedatatype.java
@@ -0,0 +1,20 @@
+public class primitivedatatype {
+    //variable declaration
+    static int a;
+    static float b;
+    static double c;
+    static String d;
+    static char e;
+    static long f;
+    static boolean g;
+    public static void main(String args[]){
+        Syestem.out.println("Default values are...");
+        Syestem.out.println("Default values of Integer is :::" +a);
+        Syestem.out.println("Default values of Float is :::" +b);
+        Syestem.out.println("Default values of Double is :::" +c);
+        Syestem.out.println("Default values of String is :::" +d);
+        Syestem.out.println("Default values of Character is :::" +e);
+        Syestem.out.println("Default values of Long is :::" +f);
+        Syestem.out.println("Default values of Boolean is :::" +g);
+    }
+}
